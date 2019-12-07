3af6ff8df03eee797811d27cb358aaacd4266608
andy oknen <andyoknen@ya.ru>
2019-12-08 01:09:35 +0300

    gitignore

--- build.cmd
+++
@@ -1,9 +0,0 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\vsdevcmd" -arch=x64
-cd c:\users\loki\Documents\klogg
-md build
-cd build
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_VERSION=X.X.X ../
-cmake --build . --config Release -j 8
-
-del klogg.exe
-move klogg_portable.exe klogg.exe
